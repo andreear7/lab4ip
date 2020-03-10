7e20899d6c633d78ecf36f694b1a50f7bbb6abdf
andreear7 <61412181+andreear7@users.noreply.github.com>
2020-03-10 09:41:04 +0200

    forward

---
+++ Emagbun.java
@@ -0,0 +1,34 @@
+import java.util.Vector;
+
+public class Emag {
+
+  public List<Distribuitor> distribuitori;
+
+  public List<Produs> produse;
+
+  public List<Curier> curieri;
+
+  public List<Utilizatori> utilizatori;
+
+    public Vector  myProduct;
+    public Vector  myProduct;
+    public Vector  myUser;
+    public Vector  myDistribuitor;
+    public Vector  myCurier;
+
+  public void administreazaCos() {
+  }
+
+  public void recomandaProduse() {
+  }
+
+  public void adaugaUtilizator() {
+  }
+
+  public void accesBazaDeDate() {
+  }
+
+  public void adaugaDistribuitor() {
+  }
+
+}
---
+++ Userbun.java
@@ -0,0 +1,51 @@
+import java.util.Vector;
+
+public class User {
+
+  public String username;
+
+  public String password;
+
+  public String email;
+
+  public String nrTelefon;
+
+  public String adresaLivrare;
+
+    public Vector  myEmag;
+
+  public void adaugaInCos() {
+  }
+
+  public void alegeCantitate() {
+  }
+
+  public void mutaInFavorite() {
+  }
+
+  public void aplicaCardCadou() {
+  }
+
+  public void cautaProdus() {
+  }
+
+  public bool logare() {
+  return null;
+  }
+
+  public void delogare() {
+  }
+
+  public void vizualizareCos() {
+  }
+
+  public void plaseazaComanda() {
+  }
+
+  public void veziCos() {
+  }
+
+  public void stergeDinCos() {
+  }
+
+}
